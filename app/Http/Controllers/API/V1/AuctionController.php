5176d261d682ec10ebaf237b7baa1f75e5f7749c
FlipFlare <trufanov.ivan.a@yandex.ru>
2021-05-02 10:37:52 +0300

    f

--- app/Http/Controllers/API/V1/AuctionController.php
+++ app/Http/Controllers/API/V1/AuctionController.php
@@ -83,7 +83,7 @@ class AuctionController extends Controller
 
     public function list()
     {
-        return AuctionLot::whereNull('finish')->orderBy('finished', 'asc')->paginate(18);
+        return AuctionLot::whereNull('finish')->orderBy('finished', 'asc')->paginate(9);
     }
 
     public function get($id)
