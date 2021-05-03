86792c9d2fb57313530f79b837bbede6edf01b65
FlipFlare <trufanov.ivan.a@yandex.ru>
2021-05-03 15:17:07 +0300

    payment fix

--- app/Http/Controllers/API/V1/PaymentController.php
+++ app/Http/Controllers/API/V1/PaymentController.php
@@ -85,7 +85,7 @@ class PaymentController extends Controller
         }
 
         $user = User::findOrFail($order->user_id);
-        if ($user->created_at->lt(Carbon::now()->subHours(12)) && count((array) $user->payments) == 0) {
+        if ($user->created_at->lt(Carbon::now()->subHours(12)) && count((array) $user->orders) == 0) {
             $bonus_percent = $bonus_percent + 0.2;
         }
 
